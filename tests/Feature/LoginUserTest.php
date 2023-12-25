bbab08d95fa831e767233c4b80ecf8dc44d05848
alia-sewar <130346497+alia-sewar@users.noreply.github.com>
2023-12-25 16:10:35 +0300

    update

--- tests/Feature/LoginUserTest.php
+++ tests/Feature/LoginUserTest.php
@@ -2,7 +2,14 @@
 
 use App\Models\User;
 
+beforeEach(function () {
+    User::create([
+        'name'       =>    'user',
+        'email'      =>  'user@app.com',
+        'password'   =>  '123456789'
 
+    ]);
+});
 
 it('user has login', function () {
     $response = $this->post('/api/login', [
